140569da54a827dad6686ff7a0f0e8d2b67e426c
longdo <longdd@coin98.finance>
2023-01-30 00:38:27 +0700

    update

--- hooks/useWeb3.ts
+++ hooks/useWeb3.ts
@@ -3,17 +3,17 @@ import Web3 from "web3";
 const useWeb3 = () => {
     let web3;
 
-    const getWeb3 = async () => {
-        if (window.ethereum) {
-            web3 = new Web3(window.ethereum);
-            await window.ethereum.enable();
-        } else {
-            const provider = new Web3.providers.HttpProvider(
-                "https://endpoints.omniatech.io/v1/bsc/testnet/public"
-            );
-            web3 = new Web3(provider);
-        }
-    };
+    // const getWeb3 = async () => {
+    //     if (window.ethereum) {
+    //         web3 = new Web3(window.ethereum);
+    //         await window.ethereum.enable();
+    //     } else {
+    //         const provider = new Web3.providers.HttpProvider(
+    //             "https://endpoints.omniatech.io/v1/bsc/testnet/public"
+    //         );
+    //         web3 = new Web3(provider);
+    //     }
+    // };
 
     return { web3 };
 };
